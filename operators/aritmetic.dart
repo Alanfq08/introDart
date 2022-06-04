6e22ade20f810936f918d759a2dc53f332fa097e
alanfq08 <alanfq08@hotmail.com>
2022-06-04 00:03:00 -0500

    adding operators

---
+++ operators/aritmetic.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  int suma=5+6+7+8;
+  double substraction=6-7+8-5.6;
+  int multiplication=5*6;
+  double division=7/7;
+  double modulo=5%3;
+  
+  print(suma);
+  print(substraction);
+  print(multiplication);
+  print(division);
+  print(modulo);
+}
